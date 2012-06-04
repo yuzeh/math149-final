5d025ea626b2ad3854a96806bcd57629aad27240
Rafi Witten <rafiwitten@gmail.com>
2012-06-04 16:28:00 -0700

    Confusion with max filtration value

--- code/testSampler.m
+++ code/testSampler.m
@@ -1,5 +1,5 @@
 class = 1;
-chooseMetric = 1;
+chooseMetric = 3;
 datasets = {'SO', 'U', 'SU'};
 samplers = {@sampleSO, @sampleU, @sampleSU};
 metrics = {@norm2mat, @norm1vec, @norm2vec};
@@ -7,8 +7,8 @@ metrics = {@norm2mat, @norm1vec, @norm2vec};
 fprintf('Working on class %s\n', datasets{class});
 
 
-n = 2;
-numExamples = 1000;
+n = 3;
+numExamples = 100;
 
 data = {};
 
@@ -18,14 +18,19 @@ end
 
 distances = zeros(numExamples, numExamples);
 
+
+chosenMetric = metrics{chooseMetric};
 for row = 1:numExamples,
-    for col = 1:numExamples,
-        distances(row,col) = metrics{chooseMetric}(data{row}-data{col});
+    rowVal = data{row};
+    for col = row:numExamples,
+        distances(row,col) = chosenMetric(rowVal-data{col});
+        distances(col,row) = distances(row,col);
     end
+    row
 end
 
 numPoints = size(distances,1);
-max_dimension = 3;
+max_dimension = n*(n-1)/2+1;
 num_landmark_points = ceil(.1*numPoints);
 num_divisions = 20;
 
@@ -34,7 +39,8 @@ metric.impl.ExplicitMetricSpace(randn(10));
 m_space = metric.impl.ExplicitMetricSpace(distances);
 
 landmark_selector = api.Plex4.createRandomSelector(m_space, num_landmark_points);
-max_filtration_value = landmark_selector.getMaxDistanceFromPointsToLandmarks();
+
+max_filtration_value = 1e-10;%landmark_selector.getMaxDistanceFromPointsToLandmarks()/5;
 
 stream = api.Plex4.createWitnessStream(landmark_selector, max_dimension, ...
     max_filtration_value, num_divisions);
@@ -46,7 +52,7 @@ persistence = api.Plex4.getModularSimplicialAlgorithm(max_dimension,2);
 
 intervals = persistence.computeIntervals(stream);
 
-options.filename = 'Not Sure, this is general';
+options.filename = sprintf('%s(%d)', datasets{class}, n);
 options.max_filtration_value = max_filtration_value;
 options.max_dimension = max_dimension - 1;
 plot_barcodes(intervals, options);
