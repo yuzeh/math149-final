3ec5ef9c4a11b6ae1ea558e0f86bdd265a153b7d
Rafi Witten <rafiwitten@gmail.com>
2012-06-04 15:01:04 -0700

    Flattened it so everything works with javaplex

--- code/testSampler.m
+++ code/testSampler.m
@@ -1,31 +1,52 @@
-function [  ] = testSampler( class, chooseMetric)
-    metric.impl.ExplicitMetricSpace(randn(10));
-    datasets = {'SO', 'U', 'SU'};
-    samplers = {@sampleSO, @sampleU, @sampleSU};
-    metrics = {@norm2mat, @norm1vec, @norm2vec};
-    
-    fprintf('Working on class %s\n', datasets{class});
-    
-    
-    n = 3;
-    numExamples = 20;
-    
-    data = {};
-    
-    for column = 1:numExamples,
-        data{end+1} = samplers{class}(n);
-    end
-    
-    distances = zeros(numExamples, numExamples);
-    
-    for row = 1:numExamples,
-        for col = 1:numExamples,
-            distances(row,col) = metrics{chooseMetric}(data{row}-data{col});
-        end
-    end
-    metric.impl.ExplicitMetricSpace(randn(10));
-    computeHomology(distances);
-    
-    
+class = 1;
+chooseMetric = 1;
+datasets = {'SO', 'U', 'SU'};
+samplers = {@sampleSO, @sampleU, @sampleSU};
+metrics = {@norm2mat, @norm1vec, @norm2vec};
+
+fprintf('Working on class %s\n', datasets{class});
+
+
+n = 2;
+numExamples = 1000;
+
+data = {};
+
+for column = 1:numExamples,
+    data{end+1} = samplers{class}(n);
 end
 
+distances = zeros(numExamples, numExamples);
+
+for row = 1:numExamples,
+    for col = 1:numExamples,
+        distances(row,col) = metrics{chooseMetric}(data{row}-data{col});
+    end
+end
+
+numPoints = size(distances,1);
+max_dimension = 3;
+num_landmark_points = ceil(.1*numPoints);
+num_divisions = 20;
+
+metric.impl.ExplicitMetricSpace(randn(10));
+
+m_space = metric.impl.ExplicitMetricSpace(distances);
+
+landmark_selector = api.Plex4.createRandomSelector(m_space, num_landmark_points);
+max_filtration_value = landmark_selector.getMaxDistanceFromPointsToLandmarks();
+
+stream = api.Plex4.createWitnessStream(landmark_selector, max_dimension, ...
+    max_filtration_value, num_divisions);
+
+
+num_simplices = stream.getSize();
+
+persistence = api.Plex4.getModularSimplicialAlgorithm(max_dimension,2);
+
+intervals = persistence.computeIntervals(stream);
+
+options.filename = 'Not Sure, this is general';
+options.max_filtration_value = max_filtration_value;
+options.max_dimension = max_dimension - 1;
+plot_barcodes(intervals, options);
