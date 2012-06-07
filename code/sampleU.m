80e0044ea94d090b29d0ae15376c56dfe30a843f
Rafi Witten <rafiwitten@gmail.com>
2012-06-06 21:01:48 -0700

    improved draft

--- code/sampleU.m
+++ code/sampleU.m
@@ -2,13 +2,7 @@ function [ U ] = sampleU( n )
 %SAMPLEU Summary of this function goes here
 %   Detailed explanation goes here
 
-M = zeros(n);
-for row=1:n
-    for col=1:n
-        M(row,col) = 0.5 * randn() + 0.5 * i * randn();
-    end
-end
-
+M = randn(n) + 1i* randn(n);
 U = qr_pos(M);
 
 end
